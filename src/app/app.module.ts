7c061125e1164cba4e7840e54a73d99ffc408a6e
Indira <indind052@gmail.com>
2019-09-11 17:55:51 +0600

    project created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
+// tslint:disable-next-line:import-spacing
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+// tslint:disable-next-line:import-spacing
+import { FormsModule }   from '@angular/forms';
+// tslint:disable-next-line:import-spacing
+import { AppComponent }   from './app.component';
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
+
+/*
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+*/
